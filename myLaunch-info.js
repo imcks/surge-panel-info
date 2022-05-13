cbedc6ac9e87c9406a2911154ce5320258a825b1
imks <imchenks@gmail.com>
2022-05-13 14:54:15 +0800

    Update myLaunch-info.js

--- myLaunch-info.js
+++ myLaunch-info.js
@@ -15,7 +15,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"Surge  已运行"+startTime,
+    title:"𝗦𝗨𝗥𝗚𝗘ᴾᴿᴼ  已运行"+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"checkmark.seal":"info.circle.fill",
    "icon-color":icon_s?"#16A951":"#FF7500"
